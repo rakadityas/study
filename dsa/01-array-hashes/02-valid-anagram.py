f9cb38a54238297c1fa9369a3aa54a8576d18671
rakadityas <rakadityas97@gmail.com>
2025-04-19 16:52:58 +0700

    feat(dsa): add valid anagram

---
+++ dsa/01-array-hashes/02-valid-anagram.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mapS = defaultdict(int)
+        mapT = defaultdict(int)
+
+        for i in range(len(s)):
+            mapS[s[i]] += 1
+        
+        for i in range(len(t)):
+            mapT[t[i]] += 1
+        
+        return mapT == mapS
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    assert solution.isAnagram("anagram", "nagaram") == True
+    assert solution.isAnagram("rat", "car") == False
+    assert solution.isAnagram("ratt", "car") == False
