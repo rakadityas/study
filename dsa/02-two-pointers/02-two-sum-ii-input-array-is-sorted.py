a82e4be5620d7844c485c4b0b414290a89ffa2b9
rakadityas <rakadityas97@gmail.com>
2025-04-19 18:29:13 +0700

    feat(dsa): two sum but sorted

---
+++ dsa/02-two-pointers/02-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def twoSum(self, numbers: list[int], target:int) -> list[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while l < r:
+            sumNumbers = numbers[l] + numbers[r]
+            if sumNumbers == target:
+                return [l+1, r+1]
+            elif sumNumbers > target:
+                r -= 1
+            else:
+                l += 1
+        
+        return [0,0]
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.twoSum([2,7,11,15], 9) == [1,2]
+    assert solution.twoSum([2,3,4], 6) == [1,3]
+    assert solution.twoSum([-1,0], -1) == [1,2]
