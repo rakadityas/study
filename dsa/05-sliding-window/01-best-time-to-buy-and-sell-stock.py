9367f375fee270dbb4216ec29dee04c2aa4e301f
rakadityas <rakadityas97@gmail.com>
2025-04-20 20:41:31 +0700

    feat(dsa): best tiem to buy and sell stock

---
+++ dsa/05-sliding-window/01-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maxProfit = max(prices[r] - prices[l], maxProfit)
+            else:
+                l = r
+            r += 1
+        
+        return maxProfit
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.maxProfit([7,1,5,3,6,4]) == 5
+    assert solution.maxProfit([7,6,4,3,1]) == 0
