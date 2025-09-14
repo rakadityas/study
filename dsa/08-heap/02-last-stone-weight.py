81510abd04c712429d47ee05b0b9e9ef65d078ab
rakadityas <rakadityas97@gmail.com>
2025-09-14 16:31:47 +0700

    feat(dsa): last stone weight

---
+++ dsa/08-heap/02-last-stone-weight.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/last-stone-weight/
+
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        for i in range(len(stones)):
+            stones[i] = stones[i] * -1
+        
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            stoneOne = heapq.heappop(stones)
+            stoneTwo = heapq.heappop(stones)
+
+            if stoneOne < stoneTwo:
+                heapq.heappush(stones, stoneOne - stoneTwo)
+                    
+        if len(stones) == 0:
+            return 0 
+
+        return stones[0] * -1
+    
+if "__name__" == "__main__":
+    solution = Solution()
+    assert solution.lastStoneWeight([2,7,4,1,8,1]) == 1
+    assert solution.lastStoneWeight([1]) == 1
+    assert solution.lastStoneWeight([1,1]) == 0
+        
