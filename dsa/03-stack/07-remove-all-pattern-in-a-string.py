de8334afcaa6972225311de9d2b23be61bb26f5c
rakadityas <rakadityas97@gmail.com>
2025-10-14 21:34:29 +0700

    feat(dsa): stack remove all pattern in a string

---
+++ dsa/03-stack/07-remove-all-pattern-in-a-string.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def removeOccurrences(self, s: str) -> str:
+        targetDict = {"ab", "bc", "cd", "dc", "cb", "ba"}
+        stack = []
+
+        for i in range(len(s)):
+            if stack and stack[-1] + s[i] in targetDict:
+                stack.pop()
+            else:
+                stack.append(s[i])
+            
+        return "".join(stack)
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.removeOccurrences("abccba") == "ca"
+    assert s.removeOccurrences("aabccba") == "aca"
+    assert s.removeOccurrences("") == ""
