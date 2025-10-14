f318e6b3269f10086b27ce96b284e38a4328b118
rakadityas <rakadityas97@gmail.com>
2025-10-14 21:20:18 +0700

    feat(remove all adjacent duplicates in string

---
+++ dsa/03-stack/06-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            if stack and stack[-1] == s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+            
+        return "".join(stack)
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.removeDuplicates("abbaca") == "ca"
+    assert s.removeDuplicates("azxxzy") == "ay"
