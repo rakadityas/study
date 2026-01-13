d108a35af5031f369b0dc4ca0d7008b500598999
rakadityas <rakadityas97@gmail.com>
2026-01-13 08:23:13 +0700

    feat(dsa): improve search matrix algorithm

--- dsa/python/04-binary-search/02-search-a-2d-matrix.py
+++ dsa/python/04-binary-search/02-search-a-2d-matrix.py
@@ -3,18 +3,23 @@
 # space complexity: O(1)
 
 class Solution:
-    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            l, r = 0, len(matrix[i])-1
-            
-            while l <= r:
-                mid = (r+l)//2
-                if target == matrix[i][mid]:
-                    return True
-                elif target > matrix[i][mid]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            val = matrix[mid // n][mid % n]
+
+            if val == target:
+                return True
+            elif val < target:
+                l = mid + 1
+            else:
+                r = mid - 1
 
         return False
 
