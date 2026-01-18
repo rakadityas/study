30d65ac3f150cf2ccbc3749f0c502b5574b67785
rakadityas <rakadityas97@gmail.com>
2026-01-18 18:36:11 +0700

    feat(dsa): fix the kth largest element in an array

--- dsa/python/10-heap/04-kth-largest-element-in-an-array.py
+++ dsa/python/10-heap/04-kth-largest-element-in-an-array.py
@@ -1,9 +1,9 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/description/
-# time complexity: O(nlogk)
-# space complexity: O(k)
 
 import heapq
 
+# time complexity: O(nlogn): n for the conversion, logn for the heap operations, because we are converting everything first then it will be nlogn
+# space complexity: O(n)
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
         for i in range(len(nums)):
@@ -16,6 +16,20 @@ class Solution:
             k = k - 1
         
         return heapq.heappop(nums) * -1
+
+# time complexity: O(nlogk): k is the size of the heap, n is the number of elements in the array
+# space complexity: O(k)
+class SolutionOptimal:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        heapq.heapify(heap)
+
+        for i in range(len(nums)):
+            heapq.heappush(heap, nums[i])
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
 
 if __name__ == "__main__":
     s = Solution()
