96474c37433f4a6f09d3e31595aa657cad7b099a
rakadityas <rakadityas97@gmail.com>
2026-01-18 17:34:29 +0700

    feat(dsa): kth largest element in a stream optimal

--- dsa/python/10-heap/01-kth-largest-element-in-a-stream.py
+++ dsa/python/10-heap/01-kth-largest-element-in-a-stream.py
@@ -24,6 +24,25 @@ class KthLargest:
             heapq.heappop(self.minHeap)
         return
 
+# removing the maintain heap functionality, uses heapreplace instead
+class KthLargestOptimal:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = nums
+
+        heapq.heapify(self.minHeap)
+
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        if len(self.minHeap) < self.k:
+            heapq.heappush(self.minHeap, val)
+        elif val > self.minHeap[0]:
+            heapq.heapreplace(self.minHeap, val)
+
+        return self.minHeap[0]
+
 if __name__ == "__main__":
     kthLargest = KthLargest(3, [4, 5, 8, 2])
     assert kthLargest.add(3) == 4
