46f188f50f29a42836561caf8b1b8803d83dfcd4
rakadityas <rakadityas97@gmail.com>
2026-07-23 18:14:40 +0700

    feat(dsa): word search update

--- dsa/python/09-backtracking/06-word-search.py
+++ dsa/python/09-backtracking/06-word-search.py
@@ -32,56 +32,7 @@ class Solution:
                 if backtracking(r, c, 0):
                     return True
         
-        return False
-class SolutionBrute: # not working really well, unfinished, and only covers the right -> bottom -> left direction
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        # right -> bottom -> left
-        lenWord = len(word)
-        self.hasFound = False
-
-        def backtrack(path: List[str], wordIdx: int, startI: int, startJ: int):
-            print(path)
-            if board[startI][startJ] != word[wordIdx]:
-                return
-            else:
-                wordIdx += 1
-
-            if lenWord == wordIdx:
-                self.hasFound = True
-
-            for i in range(startI, len(board)):
-                for j in range(startJ, len(board)):
-                    path.append(board[i][j])
-                    backtrack(path, wordIdx+1, i+1, j+1) #traverse right
-                    path.pop()
-
-                    if self.hasFound:
-                        break
-                    
-                    for iBottom in range(i, len(board)):
-                        path.append(board[iBottom][j])
-                        backtrack(path, wordIdx+1, iBottom+1, j) #traverse bottom
-                        path.pop()
-                    
-                        if self.hasFound:
-                            break
-                    
-                    for iLeft in range(i, -1, -1):
-                        path.append(board[iLeft][j])
-                        backtrack(path, wordIdx+1, iLeft, j-1) #traverse left
-                        path.pop()
-
-                        if self.hasFound:
-                            break
-                
-                if self.hasFound:
-                        break
-            return
-        
-        backtrack([], 0, 0, 0)
-
-        return self.hasFound
-    
+        return False    
 
 if __name__ == "__main__":
     board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
